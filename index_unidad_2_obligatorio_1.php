3e38f68dabf9999fcf18178dafc5357c14fa8d9d
ikalay <40150264+ikalay@users.noreply.github.com>
2021-07-14 00:10:41 -0300

    Add files via upload

---
+++ index_unidad_2_obligatorio_1.php
@@ -0,0 +1,20 @@
+
+<?php
+
+
+$persona = array
+($persona1 = array(
+
+        "nombre" => " Ignacio Kalaydjian Martinez ",
+        "edad" => 39,
+        "profesion" => "arquitecto",
+        "editor" => "Neetbeans",
+        "hobbie" => "correr",
+        "sistemaoperativo" => "OSX"
+
+));
+
+echo "Mi nombre es " .$persona1["nombre"] ." Soy " .$persona1["profesion"] ." y tengo " .$persona1["edad"] ." anios. Me gusta " .$persona1["hobbie"] .".  Para programar uso " .$persona1["editor"] ." y mi sistema seleccionado es ".$persona1["sistemaoperativo"] .".";
+
+
+?> 
